ced5d5cd8ddebf0a7d59baef76591f07055aa9be
Alex Wanyoike <alex.m.wanyoike@gmail.com>
2025-08-31 08:42:06 +0300

    Add support for music playback

--- src/scenes/FinishArea/finishArea.gd
+++ src/scenes/FinishArea/finishArea.gd
@@ -3,4 +3,4 @@ class_name FinishArea extends Area2D
 signal player_entered_finish_area
 
 func _on_body_entered(_player: Player) -> void:
-	player_entered_finish_area.emit()
+  player_entered_finish_area.emit()
